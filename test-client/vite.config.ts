295c05971b2de53d4e3e8654524d50d742722c76
TheNotary <no@email.plz>
2026-05-23 12:55:48 -0500

    fifteee centsgit add .git add .

--- test-client/vite.config.ts
+++ test-client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/solfege-detector/',
   plugins: [react()],
   test: {
     globals: true,
